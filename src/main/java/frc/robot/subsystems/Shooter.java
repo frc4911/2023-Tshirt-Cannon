7830687dcaf9c42c32fbd14bafc70916340a9d39
scottminnig <119763625+scottminnig@users.noreply.github.com>
2023-10-07 13:42:24 -0700

    Updated Shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,25 @@
 package frc.robot.subsystems;
 
-public class Shooter {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Shooter extends SubsystemBase {
+    private TalonSRX inputvalve = new TalonSRX(0);
+    private TalonSRX outputvalve = new TalonSRX(0);
+    
+    public void openinput() {
+        inputvalve.set(ControlMode.PercentOutput, 100);
+    }
+    public void closevalve() {
+        inputvalve.set(ControlMode.PercentOutput, 0);
+    } 
+    public void openoutput() {
+        outputvalve.set(ControlMode.PercentOutput, 100);
+    }
+    public void closeoutput() {
+        outputvalve.set(ControlMode.PercentOutput, 0);
+    }
     
 }
