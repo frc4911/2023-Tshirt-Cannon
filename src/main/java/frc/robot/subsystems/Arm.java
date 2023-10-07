2b72502585d910fabb5459431c177811de15af85
GavW1 <90525092+GavW1@users.noreply.github.com>
2023-10-07 13:40:03 -0700

    Update Arm.java
    added SetMotorSpeed and ReadEncoder, added encoder

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,8 +1,22 @@
 package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Arm {
+
+public class Arm extends SubsystemBase {
     private TalonSRX ArmTalon = new TalonSRX(0);
+    private AnalogInput encoder = new AnalogInput(0);
 
+    public void SetMotorSpeed(double speed) {
+        ArmTalon.set(ControlMode.PercentOutput, speed);
+
+    }
+
+    public double ReadEncoder(){
+        return encoder.getVoltage(); //probably not right
+    }
+    
 }
